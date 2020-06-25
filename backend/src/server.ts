e72fd6c5692c9a34e3a6372708a863c972535a77
Igor Quiterio <idcq101@gmail.com>
2020-06-24 23:09:59 -0300

    backend init files

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+app.use(express.json());
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
