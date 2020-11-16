c68ba8411f7495d6cb84bca45d2c4038471f9989
Igor Quiterio <idcq101@gmail.com>
2020-11-15 22:29:31 -0300

    habilitando cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -9,6 +10,7 @@ import AppError from './errors/appError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
