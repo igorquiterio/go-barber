1261ab8e83dfffe0fc2e43a5aad9ee8a59b693a1
Igor Quiterio <idcq101@gmail.com>
2020-11-26 20:44:49 -0300

    Input & Button

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
@@ -10,6 +13,17 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('oi');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
