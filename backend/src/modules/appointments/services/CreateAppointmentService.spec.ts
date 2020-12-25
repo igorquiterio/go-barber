fb69ee126baf8820829d4592555a212a9aefecd9
Igor Quiterio <idcq101@gmail.com>
2020-12-25 20:58:15 -0300

    Pensando nos testes

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {});
 });
