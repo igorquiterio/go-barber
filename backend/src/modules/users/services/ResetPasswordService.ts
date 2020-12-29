db1786a73dbce21150da591937a92cc412e807ef
Igor Quiterio <idcq101@gmail.com>
2020-12-28 21:31:39 -0300

    findByToken

---
+++ backend/src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,42 @@
+import { injectable, inject } from 'tsyringe';
+import User from '@modules/users/infra/typeorm/entities/User';
+import AppError from '@shared/errors/appError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User does not exists');
+    }
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    user.password = password;
+
+    this.usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordService;
