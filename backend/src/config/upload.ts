6b68815db31cb86ebe5fd98474a466548288254f
Igor Quiterio <idcq101@gmail.com>
2021-01-09 21:50:23 -0300

    Upload de arquivos para o Amazon S3

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,20 +1,49 @@
-import multer from 'multer';
+/* eslint-disable @typescript-eslint/ban-types */
 import crypto from 'crypto';
 import path from 'path';
+import multer, { StorageEngine } from 'multer';
 
+interface IUploadConfig {
+  driver: 'disk' | 's3';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+
+    aws: {
+      bucket: string;
+    };
+  };
+}
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const filename = `${fileHash}-${file.originalname}`;
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const filehash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${filehash}-${file.originalname}`;
 
-      return callback(null, filename);
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-2',
     },
-  }),
-};
+  },
+} as IUploadConfig;
